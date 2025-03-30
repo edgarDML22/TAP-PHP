9b99c95e88848d886e8ceae5240bfa3354c58289
Edgar <edgar.ml.2005@outlook.com>
2025-03-29 22:52:00 -0600

    login style done

---
+++ users_login_project/login.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+include 'includes/functions/signup_functions.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/styles/style_login.css">
+    <title>Log in</title>
+</head>
+<body> 
+    <div class="main_container">
+    <h1>USER LOGIN</h1>
+    <!-- mensajes de estado -->
+    <?php show_error_or_success_message()?>
+
+    <!-- un forms donde pidamos usuario y contraseña -->
+    <form action="includes/controllers/login_controller.php" method="post">
+    <div style="display: flex; align-items: center; margin-bottom: 1px; padding: 6px;">
+        <input required type="text" id="username" name="username" value="" placeholder="Username" style="vertical-align: middle;">
+        <img src="assets/images/user_logo.jpg" alt="Usuario" style="padding: 6px; width: 50px; height: 50px; border-radius: 50%; object-fit: cover;">
+    </div>
+
+    <div style="display: flex; align-items: center; margin-bottom: 6px; padding: 6px;">
+        <input required type="password" id="pass" name="pass" placeholder="Password" style="vertical-align: middle;">
+        <img src="assets/images/padlock_logo.jpg" alt="Contraseña" style="padding:6px; width: 50px; height: 50px; border-radius: 50%; object-fit: cover;">
+    </div>
+
+    <button type="submit" class="send_button">LOGIN</button>
+    </form>
+    <br><br><br>
+    </div>
+
+    <div class="signup_container">
+        <!-- Botón para ir a la página de signup -->
+        <p>You don't have an account yet? </p>
+        <a href="signup.php" class="signup-button">SIGN UP</a>
+    </div>
+    
+</body>
+</html>
